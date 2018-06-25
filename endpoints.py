766f664071971ec10505a2cc8c8abcbf4f5502de
Zemann <zemann.schps@gmail.com>
2018-06-25 17:41:22 -0400

    API-test
    -modulated the code more
    -cleaned up

--- endpoints.py
+++ endpoints.py
@@ -9,6 +9,15 @@ from functions import *
 from functools import update_wrapper
 
 
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+
+import json
+
+
+
+
 auth = HTTPBasicAuth() 
 
 app = Flask(__name__, template_folder="templates") 
@@ -162,7 +171,50 @@ def bad_request(error):
   return make_response(jsonify({'error': 'Authentication Required: Data protected.'}), 401)
 
 
-"""@app.after_request
+
+
+class RateLimit(object):
+    expiration_window = 10
+
+    def __init__(self, key_prefix, limit, per, send_x_headers):
+        self.reset = (int(time.time()) // per) * per + per
+        self.key = key_prefix + str(self.reset)
+        self.limit = limit
+        self.per = per
+        self.send_x_headers = send_x_headers
+        p = redis.pipeline()
+        p.incr(self.key)
+        p.expireat(self.key, self.reset + self.expiration_window)
+        self.current = min(p.execute()[0], limit)
+
+    remaining = property(lambda x: x.limit - x.current)
+    over_limit = property(lambda x: x.current >= x.limit)
+
+def get_view_rate_limit():
+    return getattr(g, '_view_rate_limit', None)
+
+def on_over_limit(limit):
+    return (jsonify({'data':'You hit the rate limit','error':'429'}),429)
+
+def ratelimit(limit, per=300, send_x_headers=True,
+              over_limit=on_over_limit,
+              scope_func=lambda: request.remote_addr,
+              key_func=lambda: request.endpoint):
+    def decorator(f):
+        def rate_limited(*args, **kwargs):
+            key = 'rate-limit/%s/%s/' % (key_func(), scope_func())
+            rlimit = RateLimit(key, limit, per, send_x_headers)
+            g._view_rate_limit = rlimit
+            if over_limit is not None and rlimit.over_limit:
+                return over_limit(rlimit)
+            return f(*args, **kwargs)
+        return update_wrapper(rate_limited, f)
+    return decorator
+
+
+
+
+@app.after_request
 def inject_x_rate_headers(response):
     limit = get_view_rate_limit()
     if limit and limit.send_x_headers:
@@ -173,9 +225,9 @@ def inject_x_rate_headers(response):
     return response
 
 @app.route('/rate-limited')
-@ratelimit(limit=5, per=30 * 1)
+@ratelimit(limit=30, per=60 * 1)
 def index():
-    return jsonify({'response':'This is a rate limited response'})"""
+    return jsonify({'response':'This is a rate limited response'})
 
 
 
